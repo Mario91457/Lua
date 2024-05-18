0f9747fc383dc6275ffcd95d44fe390bbccd204c
Mario91457 <126982805+Mario91457@users.noreply.github.com>
2024-05-17 23:48:37 -0500

    simplified

--- RockPaperScissors.lua
+++ RockPaperScissors.lua
@@ -1,50 +1,36 @@
 local round = 4
-local optionsforbot = {"Papel", "Piedra", "Tigera"}
+local Options = {
+    Piedra = {Gana = "Tijera", Pierde = "Papel"},
+    Papel = {Gana = "Piedra", Pierde = "Tijera"},
+    Tijera = {Gana = "Papel", Pierde = "Roca"},
+}
+local optionsforbot = {"Papel", "Piedra", "Tijera"}
 
 function GetRandomNumber()
     math.randomseed(os.time())
     return math.random(1,3)
 end
 
-function checkifwin(jugada, numero)
-    if jugada == "Papel" or jugada == "Piedra" or jugada == "Tigera" then
-        if optionsforbot[numero] == jugada then
-            return "Empate"
-
-           else
-
-            if optionsforbot[numero] == "Papel" then
-                if jugada == "Piedra" then
-                    return "No Ganastes"
-                    elseif  jugada == "Tigera" then
-                        return "OK GANAS"
-                end
-                elseif optionsforbot[numero] == "Piedra" then
-                    if jugada == "Tigera" then
-                        return "No Ganastes"
-                        elseif  jugada == "Papel" then
-                            return "OK GANAS"
-                    end  
-                elseif optionsforbot[numero] == "Tigera" then
-                    if jugada == "Papel" then
-                        return "No Ganastes"
-                        elseif  jugada == "Piedra" then
-                            return "OK GANAS"
-                    end 
-            end
-        end
+function Checkiffun(jugada, numero)
+    if not Options[jugada] then return "e" end
+    if optionsforbot[numero] == jugada then
+        return "Empate"
+    else
+        if optionsforbot[numero] == Options[jugada].Gana then
+            return "Ganastes"
         else
-            return "PIEDRA PAPEL O TIGERA HDP"
+            return "Perdistes"
+        end
     end
 end
 
-for i = 1, round do
+for _ = 1, round do
     local random = GetRandomNumber()
-    print("Elige: Piedra Papel y Tigera (recuerda es CapsSensitive yes yes)\n")
+    print("Elige: Piedra Papel y Tijera (recuerda es CapsSensitive yes yes)\n")
     local Jugada = io.read()
     Jugada =  tostring(Jugada)
 
-    local ganoOno = checkifwin(Jugada, random)
+    local ganoOno = Checkiffun(Jugada, random)
     print("Opcion que eligio el bot: " .. optionsforbot[random])
     print ("Opcion que elegistes: " .. Jugada)
     print("y " .. ganoOno)

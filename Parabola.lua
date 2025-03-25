af44e6905ee241faaf6e93113c94e6a4de368561
Mario91457 <126982805+Mario91457@users.noreply.github.com>
2025-03-24 22:03:11 -0500

    Update Parabola.lua
    more verbose

--- Parabola.lua
+++ Parabola.lua
@@ -28,22 +28,31 @@ local function Parabola(Values)
 	local a = 0.5 * Values.g
 	local b = -v0y
 	local c = -Values.height
-
     local T_max = solveQuadratic(a, b, c) or 0
-    
-	for t = 0, T_max + Values.t_increment, Values.t_increment do
+
+    local ymax = (Values.v0^2 * math.sin(thetaRad)^2) / (2 * Values.g)
+    --local T_max = math.sqrt(2*Values.height/Values.g) solo para caida
+
+	for t = 0, T_max, Values.t_increment do
 		local x = v0x * t
 		local y = Values.height + v0y * t - 0.5 * Values.g * t^2
-        
-        print(string.format("%.2fs, X: %.3fm, Y: %.3fm", t, x, y))
+        local Vy = v0y - Values.g * t 
+
+        local V = math.sqrt(v0x^2 + Vy^2)
+        local current_theta = math.deg(math.atan(Vy/v0x))
+        if y == ymax then
+            print("Altura maxima")
+        end
+        print(string.format("Tiempo: %.2fs | X: %.3fm | Y: %.3fm | Altura: %.3fm | Velocidad: %.3fm/s | Vy: %.3fm/s | Angulo: %.3f", t, x, y, y, V, Vy, current_theta))
 	end
+    print(string.format("Altura maxima (ymax): %.3fm", ymax+Values.height))
 end
 
 Parabola({
     g = 9.80665,
-    v0 = 30,
-    height = 100,
-    theta = 90,
+    v0 = 18,
+    height = 50,
+    theta = 45,
     t_increment = 0.01
 })
 
